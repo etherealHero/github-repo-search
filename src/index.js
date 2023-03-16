7aa35f71149a4a0a9020a64c420c0132b2526235
etherealHero <gutcan4312@gmail.com>
2023-03-16 16:43:37 +1000

    верстка и стили

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import { getRepos } from "./api"
 import "./style.scss"
 
-getRepos("avs_landing")
+// getRepos("avs_landing")
+
+const form = document.forms["search"]
+
+function handleSubmit(event) {
+  event.preventDefault()
+
+  const query = event.target.elements["query"].value || null
+
+  getRepos(query)
+
+  console.log(query)
+}
+
+form.addEventListener("submit", handleSubmit)
+
+// console.log(form)
